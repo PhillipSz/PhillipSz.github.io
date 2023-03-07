1fc9e7b9ebc1669064996e3748c82d5d497a242a
Phillip Szelat <phillip.szelat@stud.uni-goettingen.de>
2023-03-07 16:11:20 +0100

    add exp

---
+++ _nuxt/app.config.c1167cf8.js
@@ -0,0 +1,1 @@
+import{L as n,M as o}from"./entry.04b6c014.js";const i=n({title:"Content moderation"}),t={};o(i,t);
---
+++ _nuxt/logo.844f9fe1.js
@@ -0,0 +1,1 @@
+import"./entry.04b6c014.js";const o=""+globalThis.__publicAssetsURL("img/logo.svg");export{o as _};
